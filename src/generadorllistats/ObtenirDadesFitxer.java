2fb2e257a239b41b4837ef5c7348f427ce7024cd
Eric Gutierrez <Eric@Eric-PC>
2013-12-19 17:14:54 +0100

    versio 2

--- src/generadorllistats/ObtenirDadesFitxer.java
+++ src/generadorllistats/ObtenirDadesFitxer.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.BufferedReader;
-import java.util.ArrayList;
 
 import java.util.TreeMap;
 
@@ -14,70 +13,48 @@ public class ObtenirDadesFitxer {
 
         // Creo el fitxer que conté les paraules per analitzar
         File fitxer = new File("Assignatures.csv");
-        String[] paraules = obtenirParaulesFitxer(fitxer);
-
-        for (int i = 0; i < paraules.length; i++) {
-            System.out.println(paraules[i]);
-
-        }
-
+        obtenirParaulesFitxer(fitxer);
     }
 
-    public static String[] obtenirParaulesFitxer(File fitxer)
+    public static void obtenirParaulesFitxer(File fitxer)
             throws IOException {
         // Creem el fitxer de lectura i el buffer per a poder llegir-ho
         FileReader lecturaFitxer = new FileReader(fitxer);
         BufferedReader bufferLectura = new BufferedReader(lecturaFitxer);
 
         // Obtenim la única linia del fitxer per a llegir les paraules
-        String linia = bufferLectura.readLine();
-
-        /* 
-         while ((bufferLectura.readLine())!=null){
-         }
-         */
         TreeMap<String, TreeMap<String, Estudiant>> assignaturesEstudiant = new TreeMap<String, TreeMap<String, Estudiant>>();
-        TreeMap<String, Estudiant> estudiants = new TreeMap<String, Estudiant>();
-        
+        TreeMap<String, Estudiant> estudiants = new TreeMap<String, Estudiant>();;
+
         // Variables per l'Estudiant
         String cognomsNom, grup;
         // Creem un array de les paraules que hi han.
-        String[] paraules = linia.split("\"");
+        String linia;
+        while ((linia = bufferLectura.readLine()) != null) {
 
-        cognomsNom = paraules[1].toString();
-        grup = paraules[3].toString();
-      
-        String[] assignatures = paraules[5].split(",");
+            String[] paraules = linia.split("\"");
 
-        Estudiant estudiant = new Estudiant(cognomsNom, grup);
+            cognomsNom = paraules[1].toString();
+            grup = paraules[3].toString();
 
-        for (String aux : assignatures){
-            if (assignaturesEstudiant.containsKey(aux)) {
-                estudiants.put(estudiant.getGrup(), estudiant);
-                assignaturesEstudiant.put(aux, estudiants);
-            }
-            else{
-                
+            String[] assignatures = paraules[5].split(",");
+
+            Estudiant estudiant = new Estudiant(cognomsNom, grup);
+
+            for (String aux : assignatures) {
+                if (assignaturesEstudiant.containsKey(aux)) {
+                    estudiants.put(estudiant.getGrup(), estudiant);
+                    assignaturesEstudiant.put(aux, estudiants);
+                } else {
+                    estudiants = new TreeMap<String, Estudiant>();
+                    estudiants.put(estudiant.getGrup(), estudiant);
+                    assignaturesEstudiant.put(aux, estudiants);
+                }
             }
         }
-        
-        /* for (String i : paraules) {
-         if (comptParaules.containsKey(i)) {
-         comptParaules.put(i,0 comptParaules.get(i) + 1);
-         percentatge.put(i, (double) comptParaules.get(i) * 100
-         / paraules.length + "%");
-
-         } else {
-         comptParaules.put(i, 1);
-         percentatge.put(i, (double) comptParaules.get(i) * 100
-         / paraules.length + "%");
-         }
-         }
-         */
+        System.out.println(assignaturesEstudiant);
         // Tanquem el fitxer
         bufferLectura.close();
-
-        return arrayAssig;
     }
 
 }
