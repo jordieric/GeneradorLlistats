f6cccd64bcf600c75cf3a2989b683f91683765d8
jordi <jordi@jordi-PC>
2014-01-04 18:07:48 +0100

    Generador de llistats en XML finalitzat. Faltara pensar en el MVC.

--- src/generadorllistats/Estudiant.java
+++ src/generadorllistats/Estudiant.java
@@ -1,15 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package generadorllistats;
 
 /**
- *
+ * Classe que representa un estudiant
  * @author jordi
  */
 public class Estudiant {
-    
+
+    //Variables de l'estudiant
+    private String cognomsNom, grup;
+
+    //Constructor de l'estudiant
+    public Estudiant(String cognomsNom, String grup) {
+        this.cognomsNom = cognomsNom;
+        this.grup = grup;
+    }
+
+    //GETTERS i SETTERS
+    public String getCognomsNom() {
+        return cognomsNom;
+    }
+
+    public void setCognomsNom(String cognomsNom) {
+        this.cognomsNom = cognomsNom;
+    }
+
+    public String getGrup() {
+        return grup;
+    }
+
+    public void setGrup(String grup) {
+        this.grup = grup;
+    }
+
+    //ToString de l'Estudiant
+    @Override
+    public String toString() {
+        return "Estudiant [cognomsNom=" + cognomsNom + ", grup=" + grup + "]";
+    }
 }
